0cc84686f3d3e6e63d09dfecb72df89217f1e0ed
倪毓琪 <lucian.ni@wanin.tw>
2026-06-30 17:25:19 +0800

    feat: tutorial - tutorial

--- clock.gd
+++ clock.gd
@@ -1,4 +1,5 @@
-extends Node2D
+class_name  Clock
+extends RigidBody2D
 
 enum StartTimeMode { SYSTEM_TIME, RANDOM_TIME, FIXED_TIME, OFFSET_TIME }
 var totalSeconds:float
@@ -9,12 +10,21 @@ var totalSeconds:float
 @export var start_second := 0
 @export var time_scale := 1.0
 
-@onready var second_arm := $SecondArm as Node2D
-@onready var minute_arm := $MinuteArm as Node2D
-@onready var hour_arm := $HourArm as Node2D
+@export_group("Nodes")
+@export var collision_shape: CollisionShape2D
+@export var visualization: Node2D
+@export_subgroup("Arms")
+@export var second_arm: Node2D
+@export var minute_arm: Node2D
+@export var hour_arm: Node2D
+
+func set_uniform_scale(scale_factor:float)->void:
+	var scaleUsed = Vector2(scale_factor, scale_factor)
+	collision_shape.scale = scaleUsed
+	visualization.scale = scaleUsed
+	mass = scale_factor * scale_factor
 
 func update_time() -> void:
-		
 	second_arm.rotation = fmod(totalSeconds, 60.0) * TAU / 60.0
 	minute_arm.rotation = fmod(totalSeconds / 60.0, 60.0 ) * TAU / 60.0
 	hour_arm.rotation = fmod(totalSeconds / 3600.0, 12.0) * TAU / 12.0
--- main.gd
+++ main.gd
@@ -4,19 +4,59 @@ extends Node
 
 @export var clock_radius := 128.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+@export_group("Clock Instances")
+@export var time_scale_min := -10.0
+@export var time_scale_max := 10.0
+@export_range(0.1, 1.0) var scale_min := 0.25
+@export_range(0.1, 1.0) var scale_max := 1.0
+
+@export_group("Nodes")
+@export var bottom: Node2D
+@export var ground: Node2D
+
+
+var _windodw_width : float
+
+func _on_size_changed() -> void:
 	var window_size := get_window().size
-	($Bottom as Node2D).position.y = window_size.y
+	_windodw_width = window_size.x
+	bottom.position.y = window_size.y + ( 2.0 * clock_radius )
 	
-	var clock := clock_scene.instantiate() as Node2D
-	clock.position = Vector2(randf_range(clock_radius, window_size.x - clock_radius), 
-							clock_radius
+	ground.position = Vector2(
+		0.5 * _windodw_width,
+		window_size.y + (0.5 * clock_radius)
+	)
+	ground.scale = Vector2(_windodw_width, clock_radius)
+
+func update_clock_position()->void:
+	var clock := clock_scene.instantiate() as Clock
+	clock.start_time = Clock.StartTimeMode.RANDOM_TIME
+	clock.time_scale = randf_range(time_scale_min, time_scale_max)
+	clock.set_uniform_scale(randf_range(scale_min, scale_max))
+	clock.position = Vector2(
+					randf_range(clock_radius, _windodw_width - clock_radius), 
+					-3 * clock_radius
 					)
 	add_child(clock)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_window().size_changed.connect(_on_size_changed)
+	_on_size_changed()
+	update_clock_position()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _on_bottom_body_entered(body: Node2D) -> void:
+	print('ready to clean' + str(body.name))
+	body.queue_free()
+	pass # Replace with function body.
+
+
+func _on_spawn_timer_timeout() -> void:
+	update_clock_position()
+	pass # Replace with function body.
